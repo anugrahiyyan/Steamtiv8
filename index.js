d707866d70fe4a950d5fa5b573268889a62d6bce
Zianurrahmani <nurrahmani.zian@gmail.com>
2022-08-19 13:52:46 +0700

    hh

--- index.js
+++ index.js
@@ -1,60 +1,74 @@
 let storeData = [
     {
         id: 1,
-        namaGame: 'hallo',
+        namaGame: 'Pole Climbing',
         harga: 120000,
         genre: 'fps',
+        img: `https://img.freepik.com/premium-vector/indonesia-traditional-games-during-independence-day-panjat-pinang-pole-climbing_75776-171.jpg?w=2000`
     },
     {   
         id: 2,
-        namaGame: 'doto',
+        namaGame: 'Tug Of War',
         harga: 720000,
         genre: 'rts',
+        img: `https://png.pngtree.com/png-vector/20210731/ourlarge/pngtree-indonesia-independence-day-tarik-tambang-competition-png-image_3751292.jpg`
     },
     {
         id: 3,
-        namaGame: 'volaran',
+        namaGame: 'Baloon Breaker',
         harga: 40000,
         genre: 'fps',
-    },
+        img: `https://asset.kompas.com/data/photo/2015/02/23/2236400bom-3780x390.jpg`
+    }
+]
+
+let storeData2 = [
     {
         id: 4,
-        namaGame: 'simkota',
+        namaGame: 'Chips Eater',
         harga: 50000,
         genre: 'sim',
+        img: `https://png.pngtree.com/png-clipart/20210801/original/pngtree-makan-kerupuk-competition-illustration-png-image_6591488.jpg`
     },
     {
         id: 5,
-        namaGame: 'hancurpermen',
+        namaGame: 'Candy Breaker',
         harga: 100000,
-        genre: 'casual'
+        genre: 'casual',
+        img: `https://media.istockphoto.com/vectors/pinata-vector-id1181852999?b=1&k=20&m=1181852999&s=612x612&w=0&h=J4RQknbWWiPYtaNk_JxXzW5K3feqri6fjXC-_lg_vDs=`
     },
     {
         id: 6,
-        namaGame: 'Modern Combat 5: Blackout',
+        namaGame: 'Coins Finder',
         harga: 600000,
-        genre: 'fps'
-    },
-    {
-        id: 7,
-        namaGame: 'DOTO 3',
-        harga: 800000,
-        genre: 'rts'
-    },
-    {
-        id: 8,
-        namaGame: 'Company of Heroes',
-        harga: 520000,
-        genre: 'rts'
-    },
-    {
-        id: 9,
-        namaGame: 'Ages of Empires',
-        harga: 900000,
-        genre: 'rts'
+        genre: 'fps',
+        img: `https://png.pngtree.com/png-vector/20190411/ourlarge/pngtree-vector-coin-icon-png-image_924873.jpg`
     },
 ]
 
+let storeData3 = [
+    {
+        id: 7,
+        namaGame: 'Marbels Racing',
+        harga: 800000,
+        genre: 'rts',
+        img: `https://i.pinimg.com/originals/24/1b/70/241b709701e39c7f753fdc01935f0252.jpg`
+    },
+    {
+        id: 8,
+        namaGame: 'Clogs Racing',
+        harga: 520000,
+        genre: 'rts',
+        img: `https://cdn-2.tstatic.net/pontianak/foto/bank/images2/Tema-1-Kelas-4-Subtema-2-Pembelajaran-3.jpg`
+    },
+    {
+        id: 9,
+        namaGame: 'Shack Runner',
+        harga: 900000,
+        genre: 'rts',
+        img: `https://png.pngtree.com/png-vector/20210727/ourlarge/pngtree-balap-karung-vector-illustration-png-image_3730672.jpg`
+    },
+]
 let cart = []
 
 //input = id data game di storeData yang ingin ditambahkan ke cart
@@ -327,6 +341,157 @@ let render = () => {
     }
 render()
 
+let filtered2 = []
+
+let filter2 = () => {
+   let input = document.getElementById('input').value;
+   filtered = storeData2.filter2(item => item.namaGame.toLowerCase().includes(input.toLowerCase()))
+   render2()
+};
+
+let render2 = () => {
+    let list = document.getElementById('list2')
+    if (filtered2.length > 0) {
+      list.innerHTML = ''
+      filtered2.forEach(el => {
+        list.innerHTML += `
+          <div class="card" style="width: 18rem;">
+            <img src="${el.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${el.namaGame}</h5>
+              <p class="card-text">${el.harga}</p>
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${el.id}">Buy</button>
+            </div>
+          </div>
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal${el.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">${el.namaGame}</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  ...
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        `
+      })
+    } else {
+      storeData2.forEach(el => {
+        list.innerHTML += `
+          <div class="card" style="width: 18rem;">
+            <img src="${el.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${el.namaGame}</h5>
+              <p class="card-text">${el.harga}</p>
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${el.id}">Buy</button>
+            </div>
+          </div>
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal${el.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">${el.namaGame}</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  ...
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        `
+      })
+    }
+  }
+render2()
 
 
+let filtered3 = []
 
+let filter3 = () => {
+   let input = document.getElementById('input').value;
+   filtered = storeData3.filter3(item => item.namaGame.toLowerCase().includes(input.toLowerCase()))
+   render3()
+};
+
+let render3 = () => {
+    let list = document.getElementById('list3')
+    if (filtered3.length > 0) {
+      list.innerHTML = ''
+      filtered3.forEach(el => {
+        list.innerHTML += `
+          <div class="card" style="width: 18rem;">
+            <img src="${el.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${el.namaGame}</h5>
+              <p class="card-text">${el.harga}</p>
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${el.id}">Buy</button>
+            </div>
+          </div>
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal${el.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">${el.namaGame}</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  ...
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        `
+      })
+    } else {
+      storeData3.forEach(el => {
+        list.innerHTML += `
+          <div class="card" style="width: 18rem;">
+            <img src="${el.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${el.namaGame}</h5>
+              <p class="card-text">${el.harga}</p>
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${el.id}">Buy</button>
+            </div>
+          </div>
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal${el.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">${el.namaGame}</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  ...
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        `
+      })
+    }
+  }
+render3()
